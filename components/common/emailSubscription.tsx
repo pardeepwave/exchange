fc807309be54a938cfb9a70b888e3cedf576cb63
jatinder mahajan <jatinder.waveinfotech@gmail.com>
2023-12-18 15:50:46 +0530

    done

--- components/common/emailSubscription.tsx
+++ components/common/emailSubscription.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 const EmailSubscription = () => {
   const [email, setEmail] = useState("");
-const handleSubscription = async () => {
+const handleSubscription = async (event) => {
+  event.preventDefault();
   try {
     const response = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/subscribe`,
@@ -29,37 +30,39 @@ const handleSubscription = async () => {
     console.error("Error:", error);
   }
 };
+
 return (
   <>
     <div className="email-section">
       <div className="container">
-          <div className="row">
-              <div className="col-lg-7 col-md-6 col-12">
-                  <h2>Republic Exchange <span>Newsletter!</span></h2>
-                  <p>The standard Lorem Ipsum passage, used since the 1500sLorem ipsum dolor sit ame</p>
-              </div>
-              <div className="col-lg-5 col-md-6 col-12">
-                  <div className="input-group mb-3">
-                      <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Enter your email"
-                      aria-label="Recipient's email"
-                      aria-describedby="button-addon2"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      />
-                      <button
-                      className="right-arrow"
-                      type="button"
-                      id="button-addon2"
-                      onClick={handleSubscription}
-                      >
-                          <i className="fa-solid fa-arrow-right"></i>
-                      </button>
-                  </div>
-              </div>
+        <div className="row">
+          <div className="col-lg-7 col-md-6 col-12">
+            <h2>Republic Exchange <span>Newsletter!</span></h2>
+            <p>The standard Lorem Ipsum passage, used since the 1500sLorem ipsum dolor sit ame</p>
           </div>
+          <div className="col-lg-5 col-md-6 col-12">
+            <form onSubmit={handleSubscription}>
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control no-outline"
+                  placeholder="Enter your email"
+                  aria-label="Recipient's email"
+                  aria-describedby="button-addon2"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <button
+                  className="right-arrow"
+                  type="submit" // Change the type to submit
+                  id="button-addon2"
+                >
+                  <i className="fa-solid fa-arrow-right"></i>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   </>
